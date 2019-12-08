b90adc9cb60f770e63fdf4ebfad5718ef1d2685b
theotherprogrammingblog <theotherprogrammingblog@gmail.com>
2019-12-08 03:07:59 -0600

    updating config

---
+++ src/test/java/com/theotherprogrammingblog/spring/boot/product/service/SpringBootProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theotherprogrammingblog.spring.boot.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
